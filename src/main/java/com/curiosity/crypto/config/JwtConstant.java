6f558f2e04faf441559d5eb8abfd6dafc0b08751
Ishu Mehta <mehta.ishu14@gmail.com>
2025-07-30 00:38:18 +0530

    implemented JwtTokenValidator, created two separate Security configs for /api (protected) and /** (public) paths respectively

---
+++ src/main/java/com/curiosity/crypto/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.curiosity.crypto.config;
+
+public class JwtConstant {
+    public static final String JWT_SECRET_KEY = "crypto-trading-app";
+    public static final String JWT_TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_HEADER_STRING = "Authorization";
+}
--- src/main/java/com/curiosity/crypto/config/SecurityConfig.java
+++ src/main/java/com/curiosity/crypto/config/SecurityConfig.java
@@ -5,21 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
+                .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/signup", "/auth/test").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults()); // <-- Use Customizer here
         return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain publicApiFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .securityMatcher("/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return null;
     }
 }
