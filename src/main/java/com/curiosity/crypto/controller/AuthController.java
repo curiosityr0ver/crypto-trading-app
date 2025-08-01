d4edfe2bb2a4ef2570706c10ac2dddc6be5b5fad
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-01 15:59:27 +0530

    added login mapping

--- src/main/java/com/curiosity/crypto/controller/AuthController.java
+++ src/main/java/com/curiosity/crypto/controller/AuthController.java
@@ -4,12 +4,15 @@ import com.curiosity.crypto.config.JwtProvider;
 import com.curiosity.crypto.model.User;
 import com.curiosity.crypto.repository.UserRepository;
 import com.curiosity.crypto.respose.AuthResponse;
+import com.curiosity.crypto.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +22,9 @@ public class AuthController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
 
     @GetMapping("/test")
     public String home() {
@@ -44,6 +50,7 @@ public class AuthController {
                 user.getEmail(), user.getPassword()
         );
 
+//        marking user to be authenticated after signup, so that they don't have to login again
         SecurityContextHolder.getContext().setAuthentication(auth);
 
         String jwt = JwtProvider.generateToken(auth);
@@ -55,5 +62,48 @@ public class AuthController {
 
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
 //         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody User user) {
+
+        String username = user.getEmail(), password = user.getPassword();
+        Authentication auth;
+        try {
+            auth = authenticate(username, password);
+        } catch (BadCredentialsException e) {
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setStatus(false);
+            authResponse.setMessage(e.getMessage());
+
+            return new ResponseEntity<>(authResponse, HttpStatus.UNAUTHORIZED);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setStatus(true);
+        authResponse.setMessage("login successfully");
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+
+    private Authentication authenticate(String username, String password) {
+
+
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+
+        if (userDetails == null) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+
+        if (!password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
     }
 }
