06092d63e57fbf26ae6833a3704f76d9e92b4482
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-01 23:04:36 +0530

    started implementing two factor otp

--- src/main/java/com/curiosity/crypto/service/TwoFactorOtpServiceImpl.java
+++ src/main/java/com/curiosity/crypto/service/TwoFactorOtpServiceImpl.java
@@ -2,11 +2,29 @@ package com.curiosity.crypto.service;
 
 import com.curiosity.crypto.model.TwoFactorOTP;
 import com.curiosity.crypto.model.User;
+import com.curiosity.crypto.repository.TwoFactorOtpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
 public class TwoFactorOtpServiceImpl implements TwoFactorOtpService {
+
+    @Autowired
+    private TwoFactorOtpRepository twoFactorOtpRepository;
+
     @Override
     public TwoFactorOTP createTwoFactorOtp(User user, String otp, String jwt) {
-        return null;
+        UUID uuid = UUID.randomUUID();
+        String id  = uuid.toString();
+        TwoFactorOTP twoFactorOtp = new TwoFactorOTP();
+        twoFactorOtp.setOtp(otp);
+        twoFactorOtp.setUser(user);
+        twoFactorOtp.setId(id);
+        twoFactorOtp.setJwt(jwt);
+        twoFactorOtpRepository.save(twoFactorOtp);
+        return twoFactorOtp;
     }
 
     @Override
