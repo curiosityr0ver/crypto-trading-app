2bb3c353f7a89935f64b2a85ea8a425173ed52d3
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-03 19:42:55 +0530

    Implemented interface for walletService, started implementing Order model

--- src/main/java/com/curiosity/crypto/service/WalletService.java
+++ src/main/java/com/curiosity/crypto/service/WalletService.java
@@ -1,4 +1,14 @@
 package com.curiosity.crypto.service;
 
+import com.curiosity.crypto.model.User;
+import com.curiosity.crypto.model.Wallet;
+
+import java.math.BigDecimal;
+
 public interface WalletService {
+    Wallet getUserWallet(User user);
+    Wallet addBalance(Wallet wallet, Long amount);
+    Wallet findWalletById(Long id);
+    Wallet walletToWalletTransfer(User sender, Wallet receiverWallet, Long amount);
+    Wallet payOrderPayment(Order order, User user);
 }
