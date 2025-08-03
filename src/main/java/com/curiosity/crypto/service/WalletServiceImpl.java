db1363945fc4ed3bda8f15666460a12104548095
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-03 20:13:44 +0530

    implementing wallet service

--- src/main/java/com/curiosity/crypto/service/WalletServiceImpl.java
+++ src/main/java/com/curiosity/crypto/service/WalletServiceImpl.java
@@ -3,18 +3,35 @@ package com.curiosity.crypto.service;
 import com.curiosity.crypto.model.Order;
 import com.curiosity.crypto.model.User;
 import com.curiosity.crypto.model.Wallet;
+import com.curiosity.crypto.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class WalletServiceImpl implements WalletService {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
     @Override
     public Wallet getUserWallet(User user) {
-        return null;
+        Wallet wallet = walletRepository.findByUserId(user.getId());
+        if(wallet == null){
+            wallet = new Wallet();
+            wallet.setUser(user);
+            walletRepository.save(wallet);
+        }
+        return wallet;
     }
 
     @Override
     public Wallet addBalance(Wallet wallet, Long amount) {
-        return null;
+        BigDecimal balance = wallet.getBalance();
+        BigDecimal newBalance = balance.add(BigDecimal.valueOf(amount));
+        wallet.setBalance(newBalance);
+        return walletRepository.save(wallet);
     }
 
     @Override
