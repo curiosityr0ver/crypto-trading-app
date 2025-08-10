ffbfab8930b0218ee37f4faf0b11f7ab7711a825
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-11 00:49:43 +0530

    refractored main app

--- src/main/java/com/trading/TradingApplication.java
+++ src/main/java/com/trading/TradingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TreadingApplication {
+public class TradingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TreadingApplication.class, args);
+		SpringApplication.run(TradingApplication.class, args);
 	}
 
 }
