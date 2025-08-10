bf6a655eb941b59b1e5adacc713ffe76f6c31d67
Ishu Mehta <mehta.ishu14@gmail.com>
2025-08-10 20:54:57 +0530

    Added repository to TransactionServiceImpl

--- src/main/java/com/trading/service/TransactionServiceImpl.java
+++ src/main/java/com/trading/service/TransactionServiceImpl.java
@@ -2,9 +2,11 @@ package com.trading.service;
 
 import com.trading.entities.Wallet;
 import com.trading.entities.WalletTransation;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TransactionServiceImpl implements TransactionService {
     @Override
     public List<WalletTransation> getTransactionsByWallet(Wallet wallet) {
